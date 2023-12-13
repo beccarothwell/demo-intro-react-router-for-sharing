1ab7b902eab1dafb89217637df8021c13b811da7
Becca Rothwell <becca.rothwell@gmail.com>
2023-12-13 11:41:15 +0000

    feat: Implement React Router - add BrowserRouter to App, add Router with Routes and Route, add MainLayout with Outlet, add NavLinks to Header, add NavLinks with useParams to Product

--- src/components/products/product/product.tsx
+++ src/components/products/product/product.tsx
@@ -1,9 +1,16 @@
+import { useParams } from "react-router-dom";
 
 const Product = () => {
-   
-return(<section className='content has-text-centered'>
-    <p>Welcome to the page for product:</p>
-    <p><strong className='is-size-1'> ?? </strong></p>
-</section>)}
+  const params = useParams();
+
+  return (
+    <section className="content has-text-centered">
+      <p>Welcome to the page for product: {params.productId}</p>
+      <p>
+        <strong className="is-size-1"> {params.productId} </strong>
+      </p>
+    </section>
+  );
+};
 
 export default Product;
--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,2 +1,22 @@
-export {}
-// what goes here??
+import { Route, Routes } from "react-router-dom";
+import Home from "../home/home";
+import MainLayout from "../layouts/main_layouts";
+import About from "../about/about";
+import Products from "../products/products";
+import Help from "../help/help";
+import Product from "../products/product/product";
+import NotFound from "../not_found/not_found";
+
+const Router = () => (
+  <Routes>
+    <Route path={"/"} element={<MainLayout />}>
+      <Route index element={<Home />}></Route>
+      <Route path={"products"} element={<Products />}></Route>
+      <Route path={"products/:productId"} element={<Product />}></Route>
+      <Route path={"about"} element={<About />}></Route>
+      <Route path={"help"} element={<Help />}></Route>
+      <Route path={"*"} element={<NotFound />}></Route>
+    </Route>
+  </Routes>
+);
+export default Router;
